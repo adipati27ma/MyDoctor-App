c700de0f7cf74c456f36801c831fdb3e9940071b
adipati27ma <adipati.cs.samsung@gmail.com>
2021-01-22 15:42:10 +0700

    done slicing screen for Upload Photo

--- src/assets/illustration/index.js
+++ src/assets/illustration/index.js
@@ -1,4 +1,5 @@
 import ILLogo from './logo.svg';
 import ILGetStarted from './get-started.png';
+import ILNullPhoto from './null_photo.png';
 
-export { ILLogo, ILGetStarted };
+export { ILLogo, ILGetStarted, ILNullPhoto };
---
+++ src/pages/UploadPhoto/index.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+import { Button, Gap, Header, Link } from '../../components';
+import { ILNullPhoto, IconAddPhoto } from '../../assets';
+import { colors, fonts } from '../../utils';
+
+export default function UploadPhoto() {
+  return (
+    <View style={styles.page}>
+      <Header title="Upload Photo" />
+      <View style={styles.content}>
+        <View style={styles.profileWrapper}>
+          <View style={styles.avatarWrapper}>
+            <Image source={ILNullPhoto} style={styles.avatar} />
+            <IconAddPhoto style={styles.addPhoto} />
+          </View>
+          <Text style={styles.name}>Shayna Melinda</Text>
+          <Text style={styles.profession}>Product Designer</Text>
+        </View>
+        <View style={styles.buttonWrapper}>
+          <Button title="Upload and Continue" />
+          <Gap height={30} />
+          <Link title="Skip for this" align="center" />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: colors.white,
+    flex: 1,
+  },
+  content: {
+    paddingHorizontal: 40,
+    paddingBottom: 64,
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  profileWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonWrapper: {},
+  avatar: {
+    width: 110,
+    height: 110,
+  },
+  avatarWrapper: {
+    width: 130,
+    height: 130,
+    borderWidth: 1,
+    borderColor: colors.border,
+    borderRadius: 130 / 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  addPhoto: {
+    position: 'absolute',
+    bottom: 8,
+    right: 6,
+  },
+  name: {
+    fontSize: 24,
+    color: colors.text.primary,
+    fontFamily: fonts.primary[600],
+    textAlign: 'center',
+  },
+  profession: {
+    fontSize: 18,
+    color: colors.text.secondary,
+    fontFamily: fonts.primary[400],
+    textAlign: 'center',
+  },
+});
